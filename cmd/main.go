4fbf7a9bfb1722c6d7d45338faa6905b62aa7f18
Anton_Wombat <tagan20011002@yandex.ru>
2025-05-19 19:24:04 +0300

    initial commit

---
+++ cmd/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"alaricode/go-fiber/internal/home"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	home.NewHandler(app)
+
+	app.Listen(":3000")
+}
