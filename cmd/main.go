a2e036a7fda6fead7bf2ed85a0ed119704ddf9aa
Anton_Wombat <tagan20011002@yandex.ru>
2025-05-19 21:59:03 +0300

    started working on config

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"alaricode/go-fiber/config"
 	"alaricode/go-fiber/internal/home"
+	"log"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
+	config.Init()
+	dbConf := config.NewDatabaseConfig()
+	log.Println(dbConf)
 	app := fiber.New()
 
 	home.NewHandler(app)
